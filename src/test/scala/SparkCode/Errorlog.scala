1c4ab2fc1e6478958b816c52c45d36f8a369f70c
slputra <manishpathak8862@gmail.com>
2021-05-15 15:36:32 +0530

    First Commit - Error logging Word Count

---
+++ src/test/scala/SparkCode/Errorlog.scala
@@ -0,0 +1,12 @@
+package SparkCode
+
+import org.apache.spark.SparkContext
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+
+object Errorlog  extends App {
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  val sc = new SparkContext ("local[*]", "wordcount2")
+  
+}
