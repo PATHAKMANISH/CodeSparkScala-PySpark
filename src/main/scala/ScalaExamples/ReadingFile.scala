5c4d0f02a5590b8c6f990fb1aab30b940cda3c9b
slputra <manishpathak8862@gmail.com>
2021-06-01 20:52:35 +0530

    Read File using Scala

---
+++ src/main/scala/ScalaExamples/ReadingFile.scala
@@ -0,0 +1,13 @@
+package ScalaExamples
+import java.io.File
+import scala.io.Source
+
+object ReadingFile extends App {
+
+  val filePath = "src/main/resources/accumulator.txt"
+
+  val file = new File(filePath)
+
+  val ScalaFileContents: Iterator[String] = Source.fromFile(file).getLines()
+  ScalaFileContents.foreach(println)
+}
