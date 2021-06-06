9bb6e30be396b76693d42ab23e6ab6477df500ef
slputra <manishpathak8862@gmail.com>
2021-06-06 14:02:33 +0530

    DataFrame Writer

---
+++ src/main/scala/SparkWeek12/DataFrameWriter.scala
@@ -0,0 +1,37 @@
+package SparkWeek12
+
+import org.apache.spark.sql.SparkSession
+
+object DataFrameWriter extends App {
+
+
+
+  val spark = SparkSession.builder()
+    .appName("Application 1")
+    .master("local[*]")
+    .getOrCreate()
+
+  val ordersDf = spark.read
+    .option("header",true)
+    .option("inferSchema",true)
+    .csv("src/main/resources/orders.csv")
+/*
+  ordersDf.show(30)
+  ordersDf.printSchema()
+  scala.io.StdIn.readLine()
+  spark.stop()
+
+  ordersDf.show(30)
+  ordersDf.printSchema()
+
+*/
+  ordersDf.write
+    .format("csv")
+    // by default format is parquet format
+    .mode(saveMode = "overwrite")
+    .option("path","src/main/resources/newResult1")
+    .save()
+  scala.io.StdIn.readLine()
+  spark.stop()
+
+}
