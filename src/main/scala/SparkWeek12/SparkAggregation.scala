4e8c4adf4883c275a35061ff9916a6e78b7e8f8b
slputra <manishpathak8862@gmail.com>
2021-06-12 14:03:21 +0530

    Spark Aggregation use case 2

--- src/main/scala/SparkWeek12/SparkAggregation.scala
+++ src/main/scala/SparkWeek12/SparkAggregation.scala
@@ -16,41 +16,72 @@ object SparkAggregation extends App {
     .appName("Application 11")
     .master("local[*]")
     .getOrCreate()
+import spark.implicits._
 
-  val invoiceDf =  spark.read
+  val invoiceDf = spark.read
     .format("csv")
-      .option("header","true")
-      .option("inferSchema","true")
-      .option("path","src/main/resources/order_data.csv")
-      .load()
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .option("path", "src/main/resources/order_data.csv")
+    .load()
 
- // invoiceDf.show(false)
-//Programatic Approach - Column object expression
+
+/*
+  // invoiceDf.show(false)
+  //Programatic Approach - Column object expression
   val res1 = invoiceDf.select(
     count("*").as("RowCount"),
     sum("Quantity").as("TotalQuantity"),
     avg("UnitPrice").as("AvgPrice"),
     countDistinct("InvoiceNo").as("CountDistinct")
   )
-    res1.show(false)
+  res1.show(false)
 
-//String expression method
+  //String expression method
 
-val res2 =invoiceDf.selectExpr(
+  val res2 = invoiceDf.selectExpr(
 
     "count(*) as RowCount",
-     "count(StockCode) as StockCount" ,  // ....it will not count null values
+    "count(StockCode) as StockCount", // ....it will not count null values
 
     "sum(Quantity) as TotalQuantity",
     "avg(unitPrice) as AvgPrice",
     "count(Distinct(InvoiceNo)) as CountDistinct"
   )
   res2.show(false)
-spark.stop()
+  spark.stop()
 
   // spark Sql
 
   invoiceDf.createOrReplaceTempView("sales") // it is liketable distributed across various machine
   spark.sql("select count(*),sum(Quantity)avg(UnitPrice),count(distinct(InvoiceNo)) from sales")
-    .show()
+  .show()
+
+  // Grouping aggrgates
+  //Programatic Approach - Column object expression
+
+  val summaryDf = invoiceDf.groupBy("Country", "InvoiceNo")
+    .agg(sum("Quantity").as("TotalQuantity"),
+      sum(expr("Quantity * UnitPrice")).as("InvoiceValue"))
+
+  summaryDf.show()
+
+  //String expression method
+
+  val summaryDf2= invoiceDf.groupBy("Country", "InvoiceNo")
+    .agg(expr("sum(Quantity)as TotalQuantity"),
+      expr("sum(Quantity* UnitPrice)as InvoiceValue")).show()
+*/
+  // spark Sql
+
+   invoiceDf.createOrReplaceTempView("sales2")
+  val summaryDf3 = spark.sql(
+    """select country ,InvoiceNo,sum(Quantity) as
+      |TotalQuantity,sum(Quantity* UnitPrice)as InvoiceValue
+      |from sales2 group by Country,InvoiceNo
+      |""".stripMargin)
+  summaryDf3.show()
+
+
+
 }
